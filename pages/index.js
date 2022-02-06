0387dc28c936b2b16d6684f367527d3c739c23d2
Stefan <Stefan.Markovic950@gmail.com>
2022-02-06 11:50:36 +0100

    profile info (WIP)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import NextLink from 'next/link';
+import { Button, Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react';
+import Section from '../components/section';
+import Paragraph from '../components/paragraph';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { BioSection, BioYear } from '../components/bio';
 
 const Page = () => {
     return ( 
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
                 Hello, I'm a Developer from Sweden.
             </Box>
 
@@ -31,6 +36,42 @@ const Page = () => {
                     />
                 </Box>
             </Box>
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <Paragraph>@TO-DO: Text about me...</Paragraph>
+            <Box align="center" my={4}>
+                <NextLink href="/works">
+                    <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                        My portfolio
+                    </Button>
+                </NextLink>
+            </Box>
+            </Section>
+
+        <Section delay={0.2}>
+            <Heading as="h3" variant="section-title" >
+                Bio
+            </Heading>
+            <BioSection>
+                <BioYear>1995</BioYear>
+            Born in Jönköping, Sweden.
+            </BioSection>
+            <BioSection>
+                <BioYear>2021</BioYear>
+                Completed the Information and Communication Engineering Master's Program at Chalmers University of Technology.
+            </BioSection>
+        </Section>
+        <Section delay={0.3}>
+            <Heading as="h3" variant="section-title">
+                I like
+            </Heading>
+            <Paragraph>
+                Development, Music, Music Production, Sound Design, Blockchain
+            </Paragraph>
+        </Section>
+
         </Container>
     )
 }
